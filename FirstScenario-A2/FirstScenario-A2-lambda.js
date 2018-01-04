69ace1f940e8b3476b139345d44a0385dd86ecb6
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-04 16:06:23 +0530

    added code

--- FirstScenario-A2/FirstScenario-A2-lambda.js
+++ FirstScenario-A2/FirstScenario-A2-lambda.js
@@ -1,3 +1,14 @@
-exports.handler = function(event, context, callback){
+const ddb = AWS.DynamoDB();
+exports.handler = function (event, context, callback) {
+    ddb.put({
+        TableName: 'Students',
+        Item: { id: 1, name: 'NAME', age: '25' }
+    }, function (err, data) {
+        if (err) {
+            //handle error
+        } else {
+            //your logic goes here
+        }
+    });
 
 }
